3f83b0f9759945b02514a1268dd570ca15d06a53
Dima Hudovec <hudovecd@gmail.com>
2016-12-11 18:57:17 +0300

    Added Data change

--- Code/Diary/app/src/main/java/com/example/hudov/diary/MainActivity.java
+++ Code/Diary/app/src/main/java/com/example/hudov/diary/MainActivity.java
@@ -2,10 +2,16 @@ package com.example.hudov.diary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.CalendarView;
 import android.widget.TabHost;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +28,15 @@ public class MainActivity extends AppCompatActivity {
 
     public void to_main(View view)
     {
+
+        CalendarView calen = (CalendarView) findViewById(R.id.calendarView);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        String selectedDate = sdf.format(new Date(calen.getDate()));
+        Log.d("mLog","Data" + selectedDate);
         setContentView(R.layout.activity_main);
         set_tab();
+
+
     }
 
     public void exit(View view)
@@ -39,20 +52,21 @@ public class MainActivity extends AppCompatActivity {
 
         tabHost.setup();
         TabHost.TabSpec tabSpec = tabHost.newTabSpec("Window1");
-        tabSpec.setContent(R.id.linearLayout);
+        tabSpec.setContent(R.id.tab1);
         tabSpec.setIndicator("Shedule");
         tabHost.addTab(tabSpec);
 
         tabSpec = tabHost.newTabSpec("Window2");
-        tabSpec.setContent(R.id.linearLayout2);
+        tabSpec.setContent(R.id.tab2);
         tabSpec.setIndicator("Bells");
         tabHost.addTab(tabSpec);
 
         tabSpec = tabHost.newTabSpec("Window3");
-        tabSpec.setContent(R.id.linearLayout3);
+        tabSpec.setContent(R.id.tab3);
         tabSpec.setIndicator("Rating");
         tabHost.addTab(tabSpec);
 
         tabHost.setCurrentTab(0);
     }
+
 }
